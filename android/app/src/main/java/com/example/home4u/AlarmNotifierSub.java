fa12b09292c6dd19cc3375311081db630a85319a
Henrik L <helagro@outlook.com>
2023-04-15 17:26:25 +0200

    Create a factory for alarm notifications

--- android/app/src/main/java/com/example/home4u/AlarmNotifierSub.java
+++ android/app/src/main/java/com/example/home4u/AlarmNotifierSub.java
@@ -1,6 +1,7 @@
 package com.example.home4u;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -11,6 +12,8 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class AlarmNotifierSub implements IMqttActionListener, MqttCallback {
     public static final String MQTT_SERVER = "";
     public static final String CLIENT_ID = "";
+
+    private static final String TAG = AlarmNotifierSub.class.getName();
 
     private final MqttClient mqttClient;
 
@@ -30,12 +33,12 @@ public class AlarmNotifierSub implements IMqttActionListener, MqttCallback {
 
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-
+        Log.e(TAG, "Failed to connect to MQTT broker");
     }
 
     @Override
     public void connectionLost(Throwable cause) {
-
+        Log.e(TAG, "Connection to MQTT broker lost");
     }
 
     @Override

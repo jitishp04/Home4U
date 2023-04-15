d3c580caaddbf8ed898c1020a6d1b9a3ce096f8e
Henrik L <helagro@outlook.com>
2023-04-15 17:46:51 +0200

    Create a notification channel for alarm related notifications

---
+++ android/app/src/main/java/com/example/home4u/AlarmNotification.java
@@ -0,0 +1,38 @@
+package com.example.home4u;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmNotification {
+    public static final String CHANNEL_NAME = "Alarm notifications";
+    public static final String CHANNEL_ID = "alarm_notification_channel";
+
+
+    public static void createChannel(Context context){
+        final int importance = NotificationManager.IMPORTANCE_HIGH;
+        final NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
+
+        final NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+
+    public static Notification create(Context context){
+        final Intent intent = new Intent(context, MainActivity.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentText("Alarm has been triggered")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentIntent(pendingIntent);
+
+        return builder.build();
+    }
+
+}
---
+++ android/app/src/main/java/com/example/home4u/MyApp.java
@@ -0,0 +1,27 @@
+package com.example.home4u;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class MyApp extends Application {
+    public static final String HAS_REGISTERED_NOTIFICATION_CHANNEL = "has registered notification channel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(! hasRegisteredNotificationChannels()){
+            registerNotificationChannels();
+        }
+    }
+
+    private boolean hasRegisteredNotificationChannels(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return sharedPreferences.getBoolean(HAS_REGISTERED_NOTIFICATION_CHANNEL, false);
+    }
+
+    private void registerNotificationChannels(){
+        AlarmNotification.createChannel(this);
+    }
+}

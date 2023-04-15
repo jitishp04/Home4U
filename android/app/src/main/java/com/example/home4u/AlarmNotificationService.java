44e40e7b78c0afd5061ab2cedd959dda0d97e768
Henrik L <helagro@outlook.com>
2023-04-15 15:52:23 +0200

    Create alarm notification service

---
+++ android/app/src/main/java/com/example/home4u/AlarmNotificationService.java
@@ -0,0 +1,16 @@
+package com.example.home4u;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AlarmNotificationService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

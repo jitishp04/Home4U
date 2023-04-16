c6e91e6eaa5cfd4c86b73abcb040830f5eb57481
Henrik L <helagro@outlook.com>
2023-04-16 13:06:43 +0200

    Add method to download music info from server
    The code is based on code provided by ChatGPT. The code provides good error handling, uses a StringBuilder for improved efficiency when building the output String, and is very readable.

--- android/app/src/main/java/com/example/home4u/MusicInfoDownloader.java
+++ android/app/src/main/java/com/example/home4u/MusicInfoDownloader.java
@@ -1,4 +1,51 @@
 package com.example.home4u;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MusicInfoDownloader {
+    public static final String MUSIC_LIBRARY_URL = "";
+
+    // *Inspired by ChatGPT*
+    public JSONObject download(){
+        HttpURLConnection urlConnection = null;
+        try {
+            final URL url = new URL(MUSIC_LIBRARY_URL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            final InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            final String jsonString = readStream(in);
+            return new JSONObject(jsonString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    // *Inspired by ChatGPT*
+    private String readStream(InputStream inputStream) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        String line = reader.readLine();
+        while(line != null){
+            stringBuilder.append(line);
+            line = reader.readLine();
+        }
+
+        reader.close();
+        return stringBuilder.toString();
+    }
 }

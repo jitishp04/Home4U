b7e45e4d7034b407e07849023d381c7eed60fd9a
shiyaox <shiyaox@student.chalmers.se>
2023-04-19 19:11:59 +0200

    Remove some comments

--- detectMotion.ino
+++ detectMotion.ino
@@ -1,7 +1,7 @@
 /****************************************************************************
   Code base on "Grove - PIR Motion Sensor".
   Source: https://wiki.seeedstudio.com/Grove-PIR_Motion_Sensor/
-*****************************************************************************//*macro definitions of PIR motion sensor pin and LED pin*/
+*****************************************************************************/
 #define PIR_MOTION_SENSOR PIN_WIRE_SCL
 #include"TFT_eSPI.h"
 TFT_eSPI tft;

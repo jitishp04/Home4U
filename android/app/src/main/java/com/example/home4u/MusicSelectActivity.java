db8e0a705770698d1936d03e35b121ad08adeb15
Henrik L <helagro@outlook.com>
2023-04-20 15:15:21 +0200

    Create a listview and populate it with the songs downloaded from the server

--- android/app/src/main/java/com/example/home4u/MusicSelectActivity.java
+++ android/app/src/main/java/com/example/home4u/MusicSelectActivity.java
@@ -2,12 +2,15 @@ package com.example.home4u;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.home4u.music_info.MusicInfoDownloader;
 import com.example.home4u.music_info.MusicInfoDownloaderCallback;
+import com.example.home4u.music_info.MusicInfoParser;
 
 import org.json.JSONObject;
 
@@ -20,9 +23,18 @@ public class MusicSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_select);
 
+        downloadSongs();
+    }
+
+    private void downloadSongs(){
         MusicInfoDownloader.downloadAsync(new MusicInfoDownloaderCallback() {
             @Override
             public void onSuccess(JSONObject jsonObject) {
+                String[] songs = MusicInfoParser.getSongs(jsonObject);
+                runOnUiThread(() -> {
+                    populateSongView(songs);
+                });
+
                 Log.v(TAG, jsonObject.toString());
             }
 
@@ -31,5 +43,11 @@ public class MusicSelectActivity extends AppCompatActivity {
                 Log.e(TAG, "Failed to download music info");
             }
         });
+    }
+
+    private void populateSongView(String[] items){
+        final ListView songListView = findViewById(R.id.song_list_view);
+        final ArrayAdapter<String> songListAdapter = new ArrayAdapter<>(MusicSelectActivity.this, android.R.layout.simple_list_item_1, items);
+        songListView.setAdapter(songListAdapter);
     }
 }

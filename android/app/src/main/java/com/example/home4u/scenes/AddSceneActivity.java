b4c20f610e74d2b2f501d16a5b695acd77e9fa96
Jitish Rajankumar Padhya <jitish@student.chalmers.se>
2023-04-21 17:34:55 +0200

    update AddSceneActivity frontend v1

--- android/app/src/main/java/com/example/home4u/scenes/AddSceneActivity.java
+++ android/app/src/main/java/com/example/home4u/scenes/AddSceneActivity.java
@@ -2,11 +2,11 @@ package com.example.home4u.scenes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.home4u.R;
@@ -14,8 +14,9 @@ import com.example.home4u.R;
 public class AddSceneActivity extends AppCompatActivity {
 
     private EditText sceneNameText;
-    private TextView sceneNameTextView;
     private Button saveSceneButton;
+    @SuppressLint("UseSwitchCompatOrMaterialCode") //doesn't send warnings regarding the min SDK not allowing switch
+    private Switch alarmSwitch, modeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +24,28 @@ public class AddSceneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_scene);
 
         addSceneView();
+        alarmSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                //TODO add the function to activate alarm
+            }else{
+                //TODO disactivate the alarm
+            }
+        });
+
+        modeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                //TODO activate dark mode
+            }else{
+                //TODO light mode
+            }
+        });
 
         saveSceneButton.setOnClickListener(v -> {
             checker();
         });
     }
+
+
 
     private void checker() {
         String sceneName = sceneNameText.getText().toString();
@@ -42,8 +60,9 @@ public class AddSceneActivity extends AppCompatActivity {
 
     private void addSceneView() {
         sceneNameText = findViewById(R.id.sceneNameText);
-        sceneNameTextView = findViewById(R.id.sceneNameTextView);
         saveSceneButton = findViewById(R.id.saveSceneButton);
+        alarmSwitch = findViewById(R.id.alarmSwitch);
+        modeSwitch = findViewById(R.id.modeSwitch);
     }
 
 

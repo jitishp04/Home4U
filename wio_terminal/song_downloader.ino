6ac8224472ed860e93e6a78a609c0afb1a8d18a0
Henrik L <helagro@outlook.com>
2023-04-24 10:20:15 +0200

    Refactor song_downloader to a class

--- wio_terminal/song_downloader.ino
+++ wio_terminal/song_downloader.ino
@@ -1,59 +1,92 @@
 #undef min
-#undef Dacc
-#undef Dac
-#undef DAC
-//#include <Audio.h>
+#include <Seeed_Arduino_FreeRTOS.h>
+#include <FreeRTOS.h>
 #include <HTTPClient.h>
-//#include <play_queue.h>
-//#include <play_queue.cpp>
-#define SAMPLE_RATE 16000
-#define BLOCK_SIZE 65536
 
-uint8_t buffer[BLOCK_SIZE];
-const String SONG_LIBRARY_URL = "http://192.168.0.135:8080";//"http://home4u-fa13b.web.app/info.json";
 
-void downloadSongInfo(){
-  myLog("Started downloading song info...");
+class HTTPClient;
 
-  const String path = SONG_LIBRARY_URL + "/info.json";
+class SongDownloader{
+  private:
+    const String SONG_LIBRARY_URL = "http://192.168.0.135:8080";//"http://home4u-fa13b.web.app/info.json";
+    const String SONG_INFO_PATH = SONG_LIBRARY_URL + "/info.json";
+    const String SONGS_DIR_PATH = SONG_LIBRARY_URL + "/songs/"
 
-  HTTPClient http;
-  http.begin(path);
-  int resCode = http.GET();
+    HTTPClient http;
 
-  if(resCode == HTTP_CODE_OK){
-    myLog("Downloaded song info");
-    Serial.println(http.getString());
-  } else {
-    myLog("Failed to download song info: " + String(resCode));
-  }
-}
+    void startStreamingSong(HTTPClient* httpClient){
+      TaskHandle_t streamSongHandle;
 
-void downloadSong(String fileName){
-  myLog("Started downloading song...");
-  const String path = SONG_LIBRARY_URL + "/songs/" + fileName;
-  //AudioPlayQueue mAudio;
-  HTTPClient http;
-  bool success = http.begin(path);
-  int resCode = http.GET();
+      myLog("Starting streaming task...");
 
-  if(resCode == HTTP_CODE_OK){
-    myLog("Downloaded song");
-
-    for (int i = 0; i < 44; i++) {
-      http.getStream().read();
+      xTaskCreate(
+        streamSong, 
+        "streamSong", 
+        1024, 
+        (void*) &input, 
+        2, 
+        &streamSongHandle
+      );
     }
 
-    while (http.connected()) {
-      int bytesRead = http.getStream().readBytes(buffer, BLOCK_SIZE);
-      if (bytesRead <= 0) break;
-      
-      playBuffer(buffer, bytesRead);
+  public:
+
+    SongDownloader(){
+
     }
-    myLog("The song has been played, are your ears still working?");
-    
-  } else {
-    myLog("Failed to download song: " + String(resCode));
-  }
+
+    String getSongInfo(){
+      myLog("Started downloading song info...");
+
+      http.begin(path);
+      int resCode = http.GET();
+
+      if(resCode == HTTP_CODE_OK){
+
+        cost String songInfo = http.getString();
+        myLog("Downloaded song info\n" + songInfo);
+        return songInfo;
+
+      } else {
+
+        myLog("Failed to download song info: " + String(resCode));
+        return NULL;
+
+      }
+    }
+
+    void downloadSong(String fileName, void (*func)()){
+      myLog("Started downloading song...");
+
+      const String path = SONGS_DIR_PATH + fileName;
+
+      http.begin(path);
+      const int resCode = http.GET();
+
+      if(resCode == HTTP_CODE_OK){
+        myLog("Downloaded song");
+
+        //Ignores header info of the .wav file
+        for (int i = 0; i < 44; i++) {
+          http.getStream().read();
+        }
+
+        startStreamingSong(&http);
+
+        delay(3000);
+        playBuffer(&audioBuffer);
+        
+        myLog("The song has been played, are your ears still working?");
+        
+      } else {
+        myLog("Failed to download song: " + String(resCode));
+      }
+    }
+
+    bool readSongSample(uint8_t* outputArray){
+      if(! http.connected()) return NULL;
+
+      int bytesRead = http.getStream().readBytes(outputArray, AUDIO_BUFFER_ENQUEUE_AMT);
+    } 
 }
 

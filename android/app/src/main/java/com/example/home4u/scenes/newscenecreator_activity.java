192b2a5358594ceeffe1613d610c92ad1ef17ed0
Jitish Rajankumar Padhya <jitish@student.chalmers.se>
2023-04-26 22:27:53 +0200

    implement newscenecreator_activity from Figma frontend v2

--- android/app/src/main/java/com/example/home4u/scenes/newscenecreator_activity.java
+++ android/app/src/main/java/com/example/home4u/scenes/newscenecreator_activity.java
@@ -21,6 +21,10 @@ import android.os.Bundle;
 
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -41,29 +45,24 @@ public class newscenecreator_activity extends Activity {
 	private TextView ___ek1;
 	private TextView start;
 	private TextView end;
-	private ImageView reply;
 	private View rectangle_8;
 	private TextView scene_name;
 	private ImageView border_color;
 	private TextView set_security;
-	private View ellipse_1;
-	private View ellipse_3;
-	private TextView w;
-	private TextView w_ek1;
-	private View ellipse_4;
-	private View ellipse_2;
-	private View ellipse_5;
-	private View ellipse_6;
-	private View ellipse_7;
-	private View rectangle_11;
-	private TextView save;
-	private TextView sa;
-	private TextView su;
-	private TextView th;
-	private TextView f;
-	private TextView m;
-	private TextView t;
-
+	private Button monButton;
+	private Button wedButton;
+	private Button thuButton;
+	private Button tueButton;
+	private Button satButton;
+	private Button sunButton;
+	private Button friButton;
+	private Switch setSecuritySwitch;
+	private Switch playMusicSwitch;
+	private Button saveButton;
+	private EditText sceneNameTextInput;
+	private EditText startTimeText;
+	private EditText endTimeText;
+	private ImageButton backButtonNewScene;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -82,30 +81,26 @@ public class newscenecreator_activity extends Activity {
 		___ek1 = (TextView) findViewById(R.id.___ek1);
 		start = (TextView) findViewById(R.id.start);
 		end = (TextView) findViewById(R.id.end);
-		reply = (ImageView) findViewById(R.id.reply);
 		rectangle_8 = (View) findViewById(R.id.rectangle_8);
-		scene_name = (TextView) findViewById(R.id.scene_name);
 		border_color = (ImageView) findViewById(R.id.border_color);
 		set_security = (TextView) findViewById(R.id.set_security);
-		ellipse_1 = (View) findViewById(R.id.ellipse_1);
-		ellipse_3 = (View) findViewById(R.id.ellipse_3);
-		w = (TextView) findViewById(R.id.w);
-		w_ek1 = (TextView) findViewById(R.id.w_ek1);
-		ellipse_4 = (View) findViewById(R.id.ellipse_4);
-		ellipse_2 = (View) findViewById(R.id.ellipse_2);
-		ellipse_5 = (View) findViewById(R.id.ellipse_5);
-		ellipse_6 = (View) findViewById(R.id.ellipse_6);
-		ellipse_7 = (View) findViewById(R.id.ellipse_7);
-		rectangle_11 = (View) findViewById(R.id.rectangle_11);
-		save = (TextView) findViewById(R.id.save);
-		sa = (TextView) findViewById(R.id.sa);
-		su = (TextView) findViewById(R.id.su);
-		th = (TextView) findViewById(R.id.th);
-		f = (TextView) findViewById(R.id.f);
-		m = (TextView) findViewById(R.id.m);
-		t = (TextView) findViewById(R.id.t);
-	
-		
+
+		monButton = findViewById(R.id.monButton);
+		wedButton = findViewById(R.id.wedButton);
+		thuButton = findViewById(R.id.thuButton);
+		tueButton = findViewById(R.id.tueButton);
+		satButton = findViewById(R.id.satButton);
+		sunButton = findViewById(R.id.sunButton);
+		friButton = findViewById(R.id.friButton);
+
+		setSecuritySwitch = findViewById(R.id.setSecuritySwitch);
+		playMusicSwitch = findViewById(R.id.playMusicSwitch);
+		saveButton = findViewById(R.id.rectangle_11);
+		sceneNameTextInput = findViewById(R.id.sceneNameTextInput);
+		startTimeText = findViewById(R.id.startTimeText);
+		endTimeText = findViewById(R.id.endTimeText);
+		backButtonNewScene = findViewById(R.id.backButtonNewScene);
+
 		//custom code goes here
 	
 	}

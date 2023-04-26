2998bdbe1327a9d5770fbf7d6a79ec70cb1bbc63
Jitish Rajankumar Padhya <jitish@student.chalmers.se>
2023-04-26 17:24:56 +0200

    update AddSceneActivity frontend with newscenecreator_activity v2 using Figma

---
+++ android/app/src/main/java/com/example/home4u/scenes/newscenecreator_activity.java
@@ -0,0 +1,114 @@
+package com.example.home4u.scenes;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		newscenecreator
+	 *	@date 		Wednesday 26th of April 2023 10:42:12 AM
+	 *	@title 		Add Scene
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.example.home4u.R;
+
+public class newscenecreator_activity extends Activity {
+
+
+	private TextView new_scene;
+	private TextView set_scene_duration;
+	private View rectangle_5;
+	private View rectangle_9;
+	private View rectangle_10;
+	private TextView play_music;
+	private ImageView rectangle_7;
+	private TextView __;
+	private View rectangle_6;
+	private TextView ___ek1;
+	private TextView start;
+	private TextView end;
+	private ImageView reply;
+	private View rectangle_8;
+	private TextView scene_name;
+	private ImageView border_color;
+	private TextView set_security;
+	private View ellipse_1;
+	private View ellipse_3;
+	private TextView w;
+	private TextView w_ek1;
+	private View ellipse_4;
+	private View ellipse_2;
+	private View ellipse_5;
+	private View ellipse_6;
+	private View ellipse_7;
+	private View rectangle_11;
+	private TextView save;
+	private TextView sa;
+	private TextView su;
+	private TextView th;
+	private TextView f;
+	private TextView m;
+	private TextView t;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newscenecreator);
+
+		new_scene = (TextView) findViewById(R.id.new_scene);
+		set_scene_duration = (TextView) findViewById(R.id.set_scene_duration);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		rectangle_9 = (View) findViewById(R.id.rectangle_9);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		play_music = (TextView) findViewById(R.id.play_music);
+		rectangle_7 = (ImageView) findViewById(R.id.rectangle_7);
+		__ = (TextView) findViewById(R.id.__);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		___ek1 = (TextView) findViewById(R.id.___ek1);
+		start = (TextView) findViewById(R.id.start);
+		end = (TextView) findViewById(R.id.end);
+		reply = (ImageView) findViewById(R.id.reply);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+		scene_name = (TextView) findViewById(R.id.scene_name);
+		border_color = (ImageView) findViewById(R.id.border_color);
+		set_security = (TextView) findViewById(R.id.set_security);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		w = (TextView) findViewById(R.id.w);
+		w_ek1 = (TextView) findViewById(R.id.w_ek1);
+		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		ellipse_5 = (View) findViewById(R.id.ellipse_5);
+		ellipse_6 = (View) findViewById(R.id.ellipse_6);
+		ellipse_7 = (View) findViewById(R.id.ellipse_7);
+		rectangle_11 = (View) findViewById(R.id.rectangle_11);
+		save = (TextView) findViewById(R.id.save);
+		sa = (TextView) findViewById(R.id.sa);
+		su = (TextView) findViewById(R.id.su);
+		th = (TextView) findViewById(R.id.th);
+		f = (TextView) findViewById(R.id.f);
+		m = (TextView) findViewById(R.id.m);
+		t = (TextView) findViewById(R.id.t);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	

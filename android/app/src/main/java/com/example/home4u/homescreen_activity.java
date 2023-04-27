1aef8eb63952de86f6d2dbc6fa7dcc70a2165be4
Utkarsh <utkarshs@student.chalmers.se>
2023-04-27 22:51:00 +0530

    Successfully imported Figma design to Android Studios

---
+++ android/app/src/main/java/com/example/home4u/homescreen_activity.java
@@ -0,0 +1,88 @@
+package com.example.home4u;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		homescreen
+	 *	@date 		Thursday 27th of April 2023 04:16:55 PM
+	 *	@title 		Home Screen
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class homescreen_activity extends Activity {
+
+	
+	private View _bg__homescreen_ek2;
+	private View rectangle_21;
+	private View rectangle_81;
+	private View rectangle_51;
+	private TextView remotely_disable_your_alarm;
+	private View rectangle_41;
+	private View rectangle_61;
+	private TextView disable_alarm;
+	private TextView create_a_scene_that_controls_your_home4u_device_;
+	private TextView connect_your_phone_to_the_device_and_play_music;
+	private View rectangle_31;
+	private TextView play_music;
+	private TextView manage_scenes;
+	private ImageView media_bluetooth_on;
+	private ImageView alarm_off;
+	private ImageView router;
+	private View rectangle_71;
+	private TextView home4u;
+	private View rectangle_91;
+	private View rectangle_110;
+	private TextView song_name;
+	private TextView artist_name;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.homescreen);
+
+		
+		_bg__homescreen_ek2 = (View) findViewById(R.id._bg__homescreen_ek2);
+		rectangle_21 = (View) findViewById(R.id.rectangle_21);
+		rectangle_81 = (View) findViewById(R.id.rectangle_81);
+		rectangle_51 = (View) findViewById(R.id.rectangle_51);
+		remotely_disable_your_alarm = (TextView) findViewById(R.id.remotely_disable_your_alarm);
+		rectangle_41 = (View) findViewById(R.id.rectangle_41);
+		rectangle_61 = (View) findViewById(R.id.rectangle_61);
+		create_a_scene_that_controls_your_home4u_device_ = (TextView) findViewById(R.id.create_a_scene_that_controls_your_home4u_device_);
+		connect_your_phone_to_the_device_and_play_music = (TextView) findViewById(R.id.connect_your_phone_to_the_device_and_play_music);
+		rectangle_31 = (View) findViewById(R.id.rectangle_31);
+		play_music = (TextView) findViewById(R.id.play_music);
+		manage_scenes = (TextView) findViewById(R.id.manage_scenes);
+		media_bluetooth_on = (ImageView) findViewById(R.id.media_bluetooth_on);
+		alarm_off = (ImageView) findViewById(R.id.alarm_off);
+		router = (ImageView) findViewById(R.id.router);
+		rectangle_71 = (View) findViewById(R.id.rectangle_71);
+		home4u = (TextView) findViewById(R.id.home4u);
+		rectangle_91 = (View) findViewById(R.id.rectangle_91);
+		rectangle_110 = (View) findViewById(R.id.rectangle_110);
+		song_name = (TextView) findViewById(R.id.song_name);
+		artist_name = (TextView) findViewById(R.id.artist_name);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	

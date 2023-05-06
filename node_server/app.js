f7e4b266a62a9f2a7fc3508eb1422e28d6805885
Henrik L <helagro@outlook.com>
2023-05-06 19:16:09 +0200

    Listen at /setAlarmTriggered and store the boolean value

--- node_server/app.js
+++ node_server/app.js
@@ -1,10 +1,41 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.text())
+
 const port = 8081;
 
 
+let isAlarmTriggered = false
+
+
+// Host songs and song info
 app.use(express.static('public'));
+
+
+app.post("/setAlarmTriggered", (req, res) => {
+    const input = req.body
+
+    if(input === "true"){
+        setAlarmTriggered(true)
+        res.send(`isAlarmTriggered set to true`)
+    } else if(input === "false"){
+        setAlarmTriggered(false)
+        res.send(`isAlarmTriggered set to false`)
+    } else {
+        const errMsg = `ERR: input must be either "true” or "false”, but was "${input}"`
+
+        console.log(errMsg)
+        res.send(errMsg)
+    }
+})
+
+function setAlarmTriggered(bool){
+    isAlarmTriggered = bool
+    console.log(`isAlarmTriggered set to ${isAlarmTriggered}`)
+}
 
 
 app.listen(port, () => {

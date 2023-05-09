d56fa381766be19935978543a062ad87335658b5
Henrik L <helagro@outlook.com>
2023-05-09 08:50:03 +0200

    Improve readability of NotificationHandler

--- android/app/src/main/java/com/example/home4u/NotificationHandler.java
+++ android/app/src/main/java/com/example/home4u/NotificationHandler.java
@@ -2,17 +2,19 @@ package com.example.home4u;
 
 import static android.Manifest.permission.POST_NOTIFICATIONS;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.TIRAMISU;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 import static com.example.home4u.MyApp.spName;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.example.home4u.alarm.AlarmNotificationHandler;
 
@@ -22,20 +24,23 @@ public class NotificationHandler {
 
     // =============== NOTIFICATION PERMISSION ================
 
-    public static void handleNotificationPermission(Activity activity){
-        if(!hasNotificationPermission(activity)){
+    @SuppressLint("NewApi") // handled
+    public static void handleNotificationPermission(Activity activity) {
+        if (!hasNotificationPermission(activity)) {
             requestNotificationPermission(activity);
         }
     }
 
-    public static boolean hasNotificationPermission(Context context){
-        return
-                android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.TIRAMISU ||
-                        ContextCompat.checkSelfPermission(context, POST_NOTIFICATIONS) == PERMISSION_GRANTED;
+    public static boolean hasNotificationPermission(Context context) {
+        if(SDK_INT < TIRAMISU) {
+            return true;
+        } else {
+            return checkSelfPermission(context, POST_NOTIFICATIONS) == PERMISSION_GRANTED;
+        }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
-    private static void requestNotificationPermission(Activity activity){
+    @RequiresApi(api = TIRAMISU)
+    private static void requestNotificationPermission(Activity activity) {
         final String[] permissions = new String[]{
                 Manifest.permission.POST_NOTIFICATIONS
         };
@@ -43,27 +48,25 @@ public class NotificationHandler {
     }
 
 
-
-
     // ============ NOTIFICATION CHANNEL ===========
 
-    public static void handleNotificationChannels(Context context){
-        if(! hasRegisteredNotificationChannels(context)){
+    public static void handleNotificationChannels(Context context) {
+        if (!hasRegisteredNotificationChannels(context)) {
             registerNotificationChannels(context);
             setHasRegisteredNotificationChannels(context);
         }
     }
 
-    private static boolean hasRegisteredNotificationChannels(Context context){
+    private static boolean hasRegisteredNotificationChannels(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(spName, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(HAS_REGISTERED_CHANNEL_ID, false);
     }
 
-    private static void registerNotificationChannels(Context context){
+    private static void registerNotificationChannels(Context context) {
         AlarmNotificationHandler.createChannel(context);
     }
 
-    private static void setHasRegisteredNotificationChannels(Context context){
+    private static void setHasRegisteredNotificationChannels(Context context) {
         context.getSharedPreferences(spName, Context.MODE_PRIVATE)
                 .edit()
                 .putBoolean(HAS_REGISTERED_CHANNEL_ID, true)
